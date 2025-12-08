cd7a186f088e1a1a866f307948369c804c4baff5
guest26 <guest26dev@gmail.com>
2025-12-08 20:49:44 +0100

    Update Lecture_3.py

--- finger_exercises/Lecture_3.py
+++ finger_exercises/Lecture_3.py
@@ -5,7 +5,7 @@ Created on Mon Dec  8 18:57:01 2025
 
 @author: Itri26
 """
-
+# Finger Exercise 3
 # Assume you are given a positive integer variable named N . 
 # Write a piece of Python code that prints hello world on separate öines, N times. 
 # You can use either a while öoop or a for öoop
